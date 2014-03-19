08c88778048627265c10b0ac4eb535f0e0ded489
北川俊太 <kitagawa@timedia.co.jp>
2014-03-19 23:44:41 +0900

    Define constans

--- app.scm
+++ app.scm
@@ -1,6 +1,12 @@
 (load "./lib.scm")
 
 (define (virtual-SSC program)
+  ;constant
+  (define mem-length 8)
+  (define pc-length  5)
+  (define op-length  3)
+  (define adr-length 5)
+
   ;run program
   (let ((RAM (ready program (init-RAM))))
     (run (init-CPU) RAM)))
